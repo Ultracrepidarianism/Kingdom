37b9adf557a09271cbd829ea588d828eab39d06e
NullException <felixbourdages@gmail.com>
2020-10-17 18:55:23 -0400

    Kingdom #5 -  preventing explosion damage, including mobs and tnt

--- src/main/java/org/github/nullexceptionarg/listener/ClaimListener.java
+++ src/main/java/org/github/nullexceptionarg/listener/ClaimListener.java
@@ -5,12 +5,11 @@ import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.entity.Wither;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.Action;
-import org.bukkit.event.block.BlockBreakEvent;
-import org.bukkit.event.block.BlockExplodeEvent;
-import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.block.*;
+import org.bukkit.event.entity.EntityChangeBlockEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -21,6 +20,7 @@ import org.github.nullexceptionarg.model.db;
 import org.yaml.snakeyaml.Yaml;
 
 import java.io.File;
+import java.util.ArrayList;
 
 public class ClaimListener implements Listener {
     private Kingdom instance;
@@ -63,11 +63,11 @@ public class ClaimListener implements Listener {
     }
 
     @EventHandler
-    public void onEntityExplosion(BlockExplodeEvent e){
+    public void onEntityExplosion(EntityExplodeEvent e){
         File file;
         String worldName;
         String chunkName;
-        for(Block b  : e.blockList()){
+        for(Block b  : new ArrayList<>(e.blockList())){
             worldName = b.getWorld().getName();
             chunkName = b.getChunk().getX() + "_" + b.getChunk().getZ();
             file = new File(dataFolder,worldName + " " + chunkName + ".yml");
@@ -78,17 +78,21 @@ public class ClaimListener implements Listener {
 
     }
 
+    @EventHandler
+    public void antiWither(EntityChangeBlockEvent e) {
+        if (e.getEntity() instanceof Wither) {
+            e.setCancelled(true);
+        }
+    }
 
     @EventHandler
     public void onPlayerMove(PlayerMoveEvent e){
         if(e.getFrom().getChunk() == e.getTo().getChunk()) return;
-        System.out.println("filtered");
         String fromChunk = e.getFrom().getWorld().getName() + " " + e.getFrom().getChunk().getX() +"_"+ e.getFrom().getChunk().getZ();
         String toChunk = e.getTo().getWorld().getName() + " " + e.getTo().getChunk().getX() +"_"+ e.getTo().getChunk().getZ();
         File dataFolder = new File(instance.getDataFolder().getAbsolutePath() + File.separator + "Claims");
         File fileFrom = new File(dataFolder, fromChunk + ".yml");
         File fileTo = new File(dataFolder, toChunk + ".yml");
-        System.out.println(fileFrom.exists() + " && " + fileTo.exists());
         if(fileFrom.exists() && !fileTo.exists()){e.getPlayer().sendMessage("Entering wilderness");}
         else if(!fileFrom.exists() && fileTo.exists()){
             FileConfiguration fileConfiguration = YamlConfiguration.loadConfiguration(fileTo);
