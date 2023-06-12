6f5c2dc34f3c1283e4ecfeb6ca860ab05d2c63d6
DeveloperAnonymous <40847862+DeveloperAnonymous@users.noreply.github.com>
2023-06-12 19:53:58 -0400

    Updated HibernateUtil

--- src/main/java/ca/ultracrepidarianism/utils/HibernateUtil.java
+++ src/main/java/ca/ultracrepidarianism/utils/HibernateUtil.java
@@ -6,8 +6,8 @@ import ca.ultracrepidarianism.model.KDTown;
 import ca.ultracrepidarianism.services.sqlutil.SqlInfo;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.AvailableSettings;
 import org.hibernate.cfg.Configuration;
-import org.hibernate.cfg.Environment;
 import org.hibernate.service.ServiceRegistry;
 
 import java.util.Properties;
@@ -17,34 +17,49 @@ public class HibernateUtil {
 
     private static final SqlInfo sqlInfo = new SqlInfo();
 
+    private HibernateUtil() {
+        // This is a utility class, so it should not be instantiated.
+    }
+
+    /**
+     * This method is used to build and configure a Hibernate SessionFactory. 
+     * A SessionFactory in Hibernate is used to create Sessions, which provide an interface between the application and the database.
+     * 
+     * @return SessionFactory This returns a SessionFactory object that is fully configured to interact with the defined database.
+     */
+    public static SessionFactory buildSessionFactory() {
+        Configuration configuration = new Configuration();
+
+        // Hibernate settings equivalent to hibernate.cfg.xml's properties
+        Properties settings = new Properties();
+        settings.put(AvailableSettings.DRIVER, "com.mysql.cj.jdbc.Driver");
+        settings.put(AvailableSettings.URL, sqlInfo.getUrl());
+        settings.put(AvailableSettings.USER, sqlInfo.getUsername());
+        settings.put(AvailableSettings.PASS, sqlInfo.getPassword());
+        settings.put(AvailableSettings.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
+        settings.put(AvailableSettings.SHOW_SQL, "false");
+        settings.put(AvailableSettings.CURRENT_SESSION_CONTEXT_CLASS, "thread");
+        settings.put(AvailableSettings.HBM2DDL_AUTO, "update");
+
+        configuration.setProperties(settings);
+        configuration.addAnnotatedClass(KDClaim.class);
+        configuration.addAnnotatedClass(KDPlayer.class);
+        configuration.addAnnotatedClass(KDTown.class);
+
+        ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .applySettings(configuration.getProperties()).build();
+
+        return configuration.buildSessionFactory(serviceRegistry);
+    }
+
+    /**
+     * This method is used to get the SessionFactory object. If the SessionFactory object has not been instantiated yet, it will be instantiated.
+     * 
+     * @return SessionFactory This returns a SessionFactory object that is fully configured to interact with the defined database.
+     */
     public static SessionFactory getSessionFactory() {
         if (sessionFactory == null) {
-            try {
-                Configuration configuration = new Configuration();
-
-                // Hibernate settings equivalent to hibernate.cfg.xml's properties
-                Properties settings = new Properties();
-                settings.put(Environment.DRIVER, "com.mysql.cj.jdbc.Driver");
-                settings.put(Environment.URL, sqlInfo.getUrl());
-                settings.put(Environment.USER, sqlInfo.getUsername());
-                settings.put(Environment.PASS, sqlInfo.getPassword());
-                settings.put(Environment.DIALECT, "org.hibernate.dialect.MySQL5Dialect");
-                settings.put(Environment.SHOW_SQL, "false");
-                settings.put(Environment.CURRENT_SESSION_CONTEXT_CLASS, "thread");
-                settings.put(Environment.HBM2DDL_AUTO, "update");
-
-                configuration.setProperties(settings);
-                configuration.addAnnotatedClass(KDClaim.class);
-                configuration.addAnnotatedClass(KDPlayer.class);
-                configuration.addAnnotatedClass(KDTown.class);
-
-                ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
-                        .applySettings(configuration.getProperties()).build();
-
-                sessionFactory = configuration.buildSessionFactory(serviceRegistry);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
+            sessionFactory = buildSessionFactory();
         }
         return sessionFactory;
     }
